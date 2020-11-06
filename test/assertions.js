e20ae7593c7e01aa9b5b211ed04ce3261ee43df4
Christopher Hiller <boneskull@boneskull.com>
2020-11-06 14:38:00 -0800

    upgrade unexpected-map for 'to have size' assertion

--- test/assertions.js
+++ test/assertions.js
@@ -348,12 +348,6 @@ module.exports = {
       .addAssertion('<Set> as array <assertion>', (expect, subject) => {
         expect.errorMode = 'nested';
         expect.shift([...subject]);
-      })
-      .addAssertion(
-        '<Map> to have size <number>',
-        (expect, subject, expectedSize) => {
-          expect(subject.size === expectedSize, 'to be true');
-        }
-      );
+      });
   }
 };
